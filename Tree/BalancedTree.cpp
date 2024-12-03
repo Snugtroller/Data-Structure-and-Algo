a9fcf8aba8876d983e90c5127780e8dfffcdf463
Suman S <suman.sh01@gmail.com>
2024-12-03 19:54:27 +0530

    identical tree

--- Tree/BalancedTree.cpp
+++ Tree/BalancedTree.cpp
@@ -31,11 +31,25 @@ bool isBalanced(Node *root)
     else
         return false;
 }
+
+bool isIdentical(Node *r1, Node *r2)
+{
+    if (r1 == NULL && r2 == NULL)
+        return 1;
+    if (r1 == NULL || r2 == NULL)
+        return 0;
+    return (r1->data == r2->data) && isIdentical(r1->left, r2->left) && isIdentical(r1->right, r2->right);
+}
 int main()
 {
     Node *root = new Node(1);
     root->left = new Node(2);
     root->right = new Node(3);
-    bool balance=isBalanced(root);
-    cout<<balance;
+    Node *root2 = new Node(1);
+    root2->left = new Node(2);
+    root2->right = new Node(5);
+    bool balance = isBalanced(root);
+    bool identical = isIdentical(root, root2);
+    cout << balance << endl;
+    cout << identical;
 }
