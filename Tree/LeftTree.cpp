d4c741d3b8fd2d1399c1b64b19c007308735ff50
Suman S <suman.sh01@gmail.com>
2024-12-04 20:07:26 +0530

    bymistake

--- Tree/LeftTree.cpp
+++ Tree/LeftTree.cpp
@@ -14,21 +14,26 @@ struct Node
     }
 };
 
-void solve(Node*root,vector<int>&ans,int level){
-    if(root==NULL)return;
-    if(level==ans.size()){
+void solve(Node *root, vector<int> &ans, int level)
+{
+    if (root == NULL)
+        return;
+    if (level == ans.size())
+    {
         ans.push_back(root->data);
     }
-    solve(root->left,ans,level+1);
-    solve(root->right,ans,level+1);
+    solve(root->left, ans, level + 1);
+    solve(root->right, ans, level + 1);
 }
 
-vector<int>LeftTree(Node*root){
-    vector<int>ans;
-    solve(root,ans,0);
+vector<int> LeftTree(Node *root)
+{
+    vector<int> ans;
+    solve(root, ans, 0);
     return ans;
 }
-int main(){
+int main()
+{
     Node *root = new Node(1);
     root->left = new Node(2);
     root->right = new Node(3);
@@ -37,9 +42,10 @@ int main(){
     root->right->right = new Node(7);
     root->right->left = new Node(6);
     root->right->right->right = new Node(8);
-    vector<int>res=LeftTree(root);
-    for(int val:res){
-        cout<<val<<"";
+    vector<int> res = LeftTree(root);
+    for (int val : res)
+    {
+        cout << val << "";
     }
-    cout<<endl;
+    cout << endl;
 }
