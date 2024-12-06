73e0cb7c1ea3c966a2267126482f6cd719ef66ea
Suman S <suman.sh01@gmail.com>
2024-12-06 17:12:22 +0530

    edit

--- List/ConvertToList.cpp
+++ List/ConvertToList.cpp
@@ -1,43 +1,40 @@
-    #include <bits/stdc++.h>
-    using namespace std;
-    struct Node
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int data1, Node *next1)
     {
-    public:
-        int data;
-        Node *next;
-
-    public:
-        Node(int data1, Node *next1)
-        {
-            data = data1;
-            next = next1;
-        }
-
-    public:
-        Node(int data1)
-        {
-            data = data1;
-            next = nullptr;
-        }
-    };
-    Node*convertolist(vector<int>&arr){
-        Node*head=new Node(arr[0]);
-        Node*mover=head;
-        for(int i=1;i<arr.size();i++){
-            Node*newnode=new Node(arr[i]);
-            mover->next=newnode;
-            mover=newnode;
-        }
-        return head;
+        data = data1;
+        next = next1;
     }
-    int main()
+    Node(int data1)
     {
-        vector<int> arr = {2, 3, 4};
-        Node*head=convertolist(arr);
-        Node*temp=head;
-        while(temp!=nullptr){
-            cout<<temp->data;
-            temp=temp->next;
-        }
-
+        data = data1;
+        next = NULL;
     }
+};
+Node *ToList(vector<int> &arr)
+{
+    Node *head = new Node(arr[0]);
+    Node *temp = head;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *newNode = new Node(arr[i]);
+        temp->next = newNode;
+        temp = newNode;
+    }
+    return head;
+}
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4};
+    Node *head = ToList(arr);
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "-";
+        temp = temp->next;
+    }
+}
