074f1f8c355276cd9e9b2e0cb5c974a9d47f0305
Suman S <suman.sh01@gmail.com>
2024-12-09 20:36:07 +0530

    deleted unwanted

--- Recursion/tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-  // Printing the power set
-    for (const auto& subset : result) {
-        cout << "{ ";
-        for (int val : subset) {
-            cout << val << " ";
-        }
-        cout << "}\n";
-    }
