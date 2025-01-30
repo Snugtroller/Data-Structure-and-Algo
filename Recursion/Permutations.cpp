293744f79764b550f053df8c41a678ee752d50bb
Suman S <suman.sh01@gmail.com>
2025-01-30 08:37:00 +0530

    Permutations using swap

--- Recursion/Permutations.cpp
+++ Recursion/Permutations.cpp
@@ -19,12 +19,29 @@ void helper(vector<int> arr, vector<int> &ds, vector<bool> &freq, vector<vector<
         }
     }
 }
+// using swap method
+void helper2(vector<int> &arr, vector<vector<int>> &ans, int index)
+{
+
+    if (index == arr.size())
+    {
+        ans.push_back(arr);
+        return;
+    }
+    for (int i = index; i < arr.size(); i++)
+    {
+        swap(arr[index], arr[i]);
+        helper2(arr, ans, index + 1);
+        swap(arr[index], arr[i]);
+    }
+}
 vector<vector<int>> permute(vector<int> arr)
 {
-    vector<int> ds;
-    vector<bool> freq(arr.size(), false);
+    // vector<int> ds;
+    // vector<bool> freq(arr.size(), false);
     vector<vector<int>> res;
-    helper(arr, ds, freq, res);
+    // helper(arr, ds, freq, res);
+    helper2(arr, res, 0);
     return res;
 }
 int main()
